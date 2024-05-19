4ed11cb708e6044a112d211e5f4af07953203acf
Andrey_Marusov <marusov140@gmail.com>
2024-05-19 10:33:25 +0300

    Сделаны табы и модалка писем

--- src/FishingSimulator/index.jsx
+++ src/FishingSimulator/index.jsx
@@ -1,8 +1,65 @@
+import { useState } from "react";
+import "./index.scss";
+import { Modal } from "../Modal/index";
+
 export const FishingSimulator = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const mailList = [
+    {
+      name: "Ирина documents@official-law.site",
+      theme: "Рабочая история от Ирины",
+      firstText: `Добрый день!.Вас беспокоит Ирина, вы спрашивали меня насчет моей рабочей истории. Там довольно большой объём, поэтому я к письму не прикладываю, а загрузила на файлообменник:$`,
+      link: "https://official-law.site/process/ru/demo-eyJjb2RlIjoiMTQ1X1dvcmtfSGlzdG9yeSIsImxvY2FsZSI6InJ1In0",
+      lastText: `Проверьте, пожалуйста, все ли правильно, до нашей следующей встречи. Спасибо!
+      С уважением,Ирина`,
+    },
+    {
+      name: "Ирина documents@official-law.site",
+      theme: "Рабочая история от Ирины",
+      firstText: `Добрый день!.Вас беспокоит Ирина, вы спрашивали меня насчет моей рабочей истории. Там довольно большой объём, поэтому я к письму не прикладываю, а загрузила на файлообменник:$`,
+      link: "https://official-law.site/process/ru/demo-eyJjb2RlIjoiMTQ1X1dvcmtfSGlzdG9yeSIsImxvY2FsZSI6InJ1In0",
+      lastText: `Проверьте, пожалуйста, все ли правильно, до нашей следующей встречи. Спасибо!
+      С уважением,Ирина`,
+    },
+  ];
+
   return (
     <>
       <div>
-        <h2>Данная старница является симулятором фишинговых ссылок</h2>
+        <div>
+          <h2>Данная старница является симулятором фишинговых ссылок</h2>
+          <p>
+            Ваша задача открыть искуственно письмо и понять является ли оно
+            безопасным для вас
+          </p>
+        </div>
+        <div>
+          {mailList.map((item, index) => {
+            return (
+              <>
+                <div className="mail_item" key={index}>
+                  <button
+                    onClick={() => {
+                      setSelectedItem(item);
+                      setIsOpen(true);
+                    }}
+                    className="mail_button"
+                  >
+                    {item.name}
+                  </button>
+                  <p>новое письмо</p>
+                </div>
+              </>
+            );
+          })}
+        </div>
+        <div>
+          {isOpen && (
+            <Modal isOpen={isOpen} setIsOpen={setIsOpen} item={selectedItem} />
+          )}
+        </div>
       </div>
     </>
   );
---
+++ src/Modal/index.jsx
@@ -0,0 +1,39 @@
+import {
+  Description,
+  DialogPanel,
+  Dialog,
+  DialogTitle,
+} from "@headlessui/react";
+import "./index.scss";
+
+export const Modal = ({ isOpen, setIsOpen, item }) => {
+  return (
+    <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+      <div className="bg">
+        <DialogPanel className="popup">
+          <DialogTitle className="text-lg font-bold">{item.theme}</DialogTitle>
+          <Description>
+            <p>{item.firstText}</p>
+            <br />
+            <a
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline"
+            >
+              {item.link}
+            </a>
+            <br />
+            <p>{item.lastText}</p>
+          </Description>
+          <button
+            onClick={() => setIsOpen(false)}
+            className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Закрыть
+          </button>
+        </DialogPanel>
+      </div>
+    </Dialog>
+  );
+};
